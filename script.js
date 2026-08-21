7a5a5ab63504f448d3eff69b76dc1ca16a96dda7
ristic1043 <risticandrej08@gmail.com>
2026-08-21 11:51:06 +0200

    sve zavrseno savrseno

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let HumanScore = 0;
 const btnRock = document.querySelector("#btnRock");
 const btnPaper = document.querySelector("#btnPaper");
 const btnScissors = document.querySelector("#btnScissors");
-
+const RW =document.querySelector("#RW");
+const hS=document.querySelector("#hS");
+const cS=document.querySelector("#cS");
 function getComputerChoice(){
 let number=Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 let ComputerGet;
@@ -40,27 +42,50 @@ return HumanGet;
 }
 
 
-
-
+ hS.textContent=`Human score ${HumanScore}`;
+cS.textContent=`Robot score ${ComputerScore}`
 
 
 
 function PlayRound(HumanGet, ComputerGet){
-    let RW=0;
+   
 if(HumanGet=='Rock' && ComputerGet=='Scissors' ||HumanGet=='Paper' && ComputerGet=='Rock' || HumanGet=='Scissors' && ComputerGet=='Paper'){
      HumanScore+=1;
+    
+
     console.log(`YOU WIN!!! ${HumanGet} beats ${ComputerGet}`); 
+    RW.textContent=`YOU WIN!!! ${HumanGet} beats ${ComputerGet}`;
+     hS.textContent=`Human score ${HumanScore}`;
+cS.textContent=`Robot score ${ComputerScore}`
+if(HumanScore==5){
+    alert("Human won");
+     window.location.reload();
+}
 }
 else if(HumanGet==ComputerGet){
     HumanScore+=0;
     ComputerScore+=0;
     console.log("TIE!!! nobody wins");
+    RW.textContent="TIE!!! nobody wins"
+     hS.textContent=`Human score ${HumanScore}`;
+cS.textContent=`Robot score ${ComputerScore}`
 }
 else{
     ComputerScore+=1;
     
      console.log(`YOU LOSE!!! ${ComputerGet} beats ${HumanGet}`); 
+     RW.textContent=`YOU LOSE!!! ${ComputerGet} beats ${HumanGet}`;
+ hS.textContent=`Human score ${HumanScore}`;
+cS.textContent=`Robot score ${ComputerScore}`
+if(ComputerScore==5){
+    alert("Computer won");
+     window.location.reload();
 }
+    }
+
+
+
+
 
 }
 
